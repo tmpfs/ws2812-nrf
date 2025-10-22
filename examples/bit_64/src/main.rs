85999ed33c46279fc2e12d3751e529c1f222ac08
muji <muji@tmpfs.org>
2025-10-22 09:55:56 +0800

    Add code to test 64 bit LED board.

---
+++ examples/bit_64/src/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::peripherals;
+use embassy_nrf::{bind_interrupts, rng};
+use embassy_nrf_ws2812_pwm::Ws2812;
+use embassy_time::Timer;
+use smart_leds::colors;
+use smart_leds::{SmartLedsWriteAsync as _, brightness};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    RNG => rng::InterruptHandler<peripherals::RNG>;
+    EGU0_SWI0 => nrf_sdc::mpsl::LowPrioInterruptHandler;
+    CLOCK_POWER => nrf_sdc::mpsl::ClockInterruptHandler;
+    RADIO => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    TIMER0 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    RTC0 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+});
+
+const NUM_LEDS: usize = 64;
+const BUFFER_SIZE: usize = NUM_LEDS * 24;
+static LED_BUFFER: StaticCell<[u16; BUFFER_SIZE]> = StaticCell::new();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+
+    let buf = LED_BUFFER.init([0u16; BUFFER_SIZE]);
+    let mut ws: Ws2812<_> = Ws2812::new(p.PWM0, p.P0_13, buf);
+
+    loop {
+        let data = [colors::BLUE; NUM_LEDS];
+        ws.write(brightness(data.into_iter(), 20)).await.unwrap();
+
+        Timer::after_secs(5).await
+    }
+}
